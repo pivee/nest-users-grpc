4efcb73bb5ce431ecf56ef29c5d813b88df5f73b
Pivithuru Jayakody <piveedj@gmail.com>
2021-02-23 13:17:24 +0530

    Configure Postgres using Nest Config

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
